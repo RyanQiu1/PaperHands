d11da5fd064f9054920fdab75c17a10bb3ce43af
ryanq <superlativeapps@gmail.com>
2022-05-29 00:39:18 +0800

    Changed to TradingviewApi

--- page/client/src/components/Dashboard/Chart.jsx
+++ page/client/src/components/Dashboard/Chart.jsx
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, Component } from "react";
 import Title from "../Template/Title.jsx";
 import LineChart from "../Template/LineChart";
 import Axios from "axios";
 import config from "../../config/Config";
-
+import TradingViewWidget, { Themes } from 'react-tradingview-widget';
+/** 
 const Chart = () => {
   const [chartData, setChartData] = useState(undefined);
 
@@ -18,19 +19,45 @@ const Chart = () => {
     getData();
   }, []);
 
+
   return (
     <React.Fragment>
       {chartData && (
         <div style={{ minHeight: "240px" }}>
           <Title>Explore {chartData.name}'s Stock Chart</Title>
+
           <LineChart
             pastDataPeriod={chartData.data}
             stockInfo={{ ticker: chartData.ticker }}
             duration={"3 years"}
           />
+
+          new Example
         </div>
       )}
     </React.Fragment>
+  );
+};**/
+
+
+const Chart = () => {
+  const tradingRef = useRef(null);
+  const [symbol, setSymbol] = useState("AAPL");
+
+  useEffect(() => {
+    console.log(tradingRef.current.props.symbol);
+  }, []);
+
+  console.log(symbol);
+
+  return (
+    <TradingViewWidget
+      symbol={symbol}
+      theme={Themes.LIGHT}
+      locale="en"
+      ref={tradingRef}
+      autosize
+    />
   );
 };
 
