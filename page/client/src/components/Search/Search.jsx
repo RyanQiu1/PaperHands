c0ae5a77cca72970b49c16e506d3dd5c7b98f748
Pan Zai Ming <zaiming.pan@gmail.com>
2022-06-12 11:15:41 +0800

    Search Page Updated
    Added searching function for coins information

--- page/client/src/components/Search/Search.jsx
+++ page/client/src/components/Search/Search.jsx
@@ -1,4 +1,3 @@
-import React, { useState, useEffect, useContext } from "react";
 import UserContext from "../../context/UserContext";
 import { TextField, Container, Grid, Box, Card } from "@material-ui/core/";
 import Autocomplete, {
@@ -8,219 +7,71 @@ import { makeStyles } from "@material-ui/core/styles";
 import LineChart from "../Template/LineChart";
 import BarChart from "./BarChart";
 import Copyright from "../Template/Copyright";
-import styles from "./Search.module.css";
-import Axios from "axios";
 import InfoCard from "./InfoCard";
 import PriceCard from "./PriceCard";
 import PurchaseCard from "./PurchaseCard";
 import PurchaseModal from "./PurchaseModal";
 import config from "../../config/Config";
 
-const filter = createFilterOptions();
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import styles from "./Search.module.css";
+import Coin from './Coin';
 
-const useStyles = makeStyles((theme) => ({
-  paper: {
-    padding: theme.spacing(2),
-    display: "flex",
-    overflow: "auto",
-    flexDirection: "column",
-    marginBottom: "40px",
-  },
-}));
-
-const LineChartCard = ({ pastDataPeriod, stockInfo, duration }) => {
-  return (
-    <Grid
-      item
-      xs={12}
-      sm={7}
-      component={Card}
-      className={styles.card}
-      style={{ minHeight: "350px" }}
-    >
-      <LineChart
-        pastDataPeriod={pastDataPeriod}
-        stockInfo={stockInfo}
-        duration={duration}
-      />
-    </Grid>
-  );
-};
-
-const BarChartCard = ({ sixMonthAverages, stockInfo }) => {
-  return (
-    <Grid item xs={12} sm component={Card} className={styles.card}>
-      <BarChart sixMonthAverages={sixMonthAverages} stockInfo={stockInfo} />
-    </Grid>
-  );
-};
-
-const StockCard = ({ setPurchasedStocks, purchasedStocks, currentStock }) => {
-  const { userData } = useContext(UserContext);
-  const [selected, setSelected] = useState(false);
-  const [stockInfo, setStockInfo] = useState(undefined);
-  const [sixMonthAverages, setSixMonthAverages] = useState(undefined);
-  const [pastDay, setPastDay] = useState(undefined);
-  const [pastMonth, setPastMonth] = useState(undefined);
-  const [pastTwoYears, setPastTwoYears] = useState(undefined);
+const Search = () => {
+  const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
-    const getInfo = async () => {
-      const url = config.base_url + `/api/data/prices/${currentStock.ticker}`;
-      const response = await Axios.get(url);
-      if (response.data.status === "success") {
-        setStockInfo(response.data.data);
-      }
-    };
-
-    getInfo();
-
-    const getData = async () => {
-      const url = config.base_url + `/api/data/prices/${currentStock.ticker}/full`;
-      const response = await Axios.get(url);
-      if (response.data.status === "success") {
-        setSixMonthAverages(response.data.sixMonthAverages);
-        setPastDay(response.data.pastDay);
-        setPastMonth(response.data.pastMonth);
-        setPastTwoYears(response.data.pastTwoYears);
-      }
-    };
-
-    getData();
+    axios
+      .get(
+        'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+      )
+      .then(res => {
+        setCoins(res.data);
+        console.log(res.data);
+      })
+      .catch(error => console.log(error));
   }, []);
 
+  const handleChange = e => {
+    setSearch(e.target.value);
+  };
+
+  const filteredCoins = coins.filter(coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
   return (
-    <div className={styles.root}>
-      {stockInfo && pastDay && (
-        <InfoCard stockInfo={stockInfo} price={pastDay.adjClose} />
-      )}
-      {sixMonthAverages && pastDay && pastMonth && pastTwoYears && (
-        <div>
-          <Grid container spacing={3}>
-            <LineChartCard
-              pastDataPeriod={pastTwoYears}
-              stockInfo={stockInfo}
-              duration={"2 years"}
-            />
-            <BarChartCard
-              sixMonthAverages={sixMonthAverages}
-              stockInfo={stockInfo}
-            />
-          </Grid>
-          <PriceCard pastDay={pastDay} />
-          <Grid container spacing={3}>
-            <PurchaseCard
-              setSelected={setSelected}
-              balance={userData.user.balance}
-            />
-            <LineChartCard
-              pastDataPeriod={pastMonth}
-              stockInfo={stockInfo}
-              duration={"month"}
-            />
-          </Grid>
-          <Box pt={4}>
-            <Copyright />
-          </Box>
-          {selected && (
-            <PurchaseModal
-              stockInfo={stockInfo}
-              pastDay={pastDay}
-              setSelected={setSelected}
-              setPurchasedStocks={setPurchasedStocks}
-              purchasedStocks={purchasedStocks}
-            />
-          )}
-        </div>
-      )}
+    <div className='coin-app'>
+      <div className='coin-search'>
+        <h1 className='coin-text'>Search Coin</h1>
+
+        <form>
+          <input
+            className='coin-input'
+            type='text'
+            onChange={handleChange}
+            placeholder='Search'
+          />
+        </form>
+      </div>
+      {filteredCoins.map(coin => {
+        return (
+          <Coin
+            key={coin.id}
+            name={coin.name}
+            price={coin.current_price}
+            symbol={coin.symbol}
+            marketcap={coin.market_cap}
+            volume={coin.total_volume}
+            image={coin.image}
+            priceChange={coin.price_change_percentage_24h}
+          />
+        );
+      })}
     </div>
   );
 };
-
-const Search = ({ setPurchasedStocks, purchasedStocks }) => {
-  const classes = useStyles();
-  const [value, setValue] = useState(null);
-  const [currentStock, setCurrentStock] = useState(null);
-
-  const onSearchChange = (event, newValue) => {
-    setValue(newValue);
-    if (newValue) {
-      setCurrentStock(newValue);
-    } else {
-      setCurrentStock(null);
-    }
-  };
-
-  return (
-    <Container className={classes.addMargin}>
-      <Autocomplete
-        value={value}
-        onChange={onSearchChange}
-        filterOptions={(options, params) => {
-          let filtered = filter(options, params);
-          if (currentStock) {
-            filtered = filtered.slice(0, 4);
-          }
-          return filtered;
-        }}
-        selectOnFocus
-        clearOnBlur
-        handleHomeEndKeys
-        id="stock-search-bar"
-        options={stocks}
-        getOptionLabel={(option) => {
-          return option.name;
-        }}
-        renderOption={(option) => option.name}
-        style={{
-          maxWidth: "700px",
-          margin: "30px auto",
-          marginBottom: "60px",
-        }}
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            label="Search for a stock"
-            variant="outlined"
-          />
-        )}
-      />
-      {currentStock && (
-        <StockCard
-          setPurchasedStocks={setPurchasedStocks}
-          purchasedStocks={purchasedStocks}
-          currentStock={currentStock}
-        />
-      )}
-      <br />
-      <br />
-      <br />
-    </Container>
-  );
-};
-
-const stocks = [
-  { name: "Apple", ticker: "AAPL" },
-  { name: "Amazon", ticker: "AMZN" },
-  { name: "Google", ticker: "GOOG" },
-  { name: "Microsoft", ticker: "MSFT" },
-  { name: "Walmart", ticker: "WMT" },
-  { name: "Intel", ticker: "INTC" },
-  { name: "American Express", ticker: "AXP" },
-  { name: "Boeing", ticker: "BA" },
-  { name: "Cisco", ticker: "CSCO" },
-  { name: "Goldman Sachs", ticker: "GS" },
-  { name: "Johson & Johnson", ticker: "JNJ" },
-  { name: "Coca-Cola", ticker: "KO" },
-  { name: "McDonald's", ticker: "MCD" },
-  { name: "Nike", ticker: "NKE" },
-  { name: "Procter & Gamble", ticker: "PG" },
-  { name: "Verizon", ticker: "VZ" },
-  { name: "Salesforce", ticker: "CRM" },
-  { name: "Visa", ticker: "V" },
-  { name: "UnitedHealth", ticker: "UNH" },
-  { name: "IBM", ticker: "IBM" },
-  { name: "Chevron", ticker: "CVX" },
-];
 
 export default Search;
